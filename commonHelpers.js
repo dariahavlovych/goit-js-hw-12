5ffa73afed83229186b7fbbbae6f7e5f40826a14
dariahavlovych <daria.havlovych@gmail.com>
2024-07-24 18:11:02 +0000

    Deploying to gh-pages from @ dariahavlovych/goit-js-hw-11@a2bd9664278e183873e0ec8b301bddeea39e2fa4 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,10 +1,16 @@
-import{S as n,i as c}from"./assets/vendor-8c59ed88.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function t(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=t(e);fetch(e.href,r)}})();const l=document.querySelector("ul.gallery");function u(s){l.innerHTML=s.map(({webformatURL:o,largeImageURL:t,tags:i,likes:e,views:r,comments:a,downloads:y})=>`<li class="gallery-item">
-	<a class="gallery-link" href="${t}">
+import{S as u,i as d}from"./assets/vendor-8c59ed88.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const m=document.querySelector("ul.gallery");function f(o){m.innerHTML=o.map(({webformatURL:r,largeImageURL:i,tags:e,likes:t,views:a,comments:n,downloads:c})=>`<li class="gallery-item">
+	<a class="gallery-link" href="${i}">
 		<img
 			class="gallery-image"
-			src="${o}"
-			alt="${i}"
+			src="${r}"
+			alt="${e}"
 			/>
+    <div class="image-stats">
+  <p>Likes </br> ${t}</p>
+  <p> Views </br> ${a}</p>
+  <p>Comments </br> ${n}</p>
+  <p>Dowloads </br> ${c}</p>
+</div>
 	</a>
-</li>`).join(""),new n(".gallery-item a",{className:"js-lightbox",overlayOpacity:.8,captionsData:"alt",captionDelay:250})}function m(s){const o=new URLSearchParams({key:"45077643-3bb964d60cb084522e3de8d92",q:s,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`https://pixabay.com/api/?${o}`).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()})}const f=document.querySelector(".search-form");f.addEventListener("submit",h);function h(s){s.preventDefault();const o=s.target.elements.search.value.trim().toLowerCase();o&&m(o).then(t=>{t.hits.length===0&&c.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",timeout:2e3,icon:""}),u(t.hits)}).catch(t=>console.log(t)).finally(s.target.reset())}
+</li>`).join(""),new u(".gallery-item a",{className:"js-lightbox",overlayOpacity:.8,captionsData:"alt",captionDelay:250}).refresh()}function h(o){const s=new URLSearchParams({key:"45077643-3bb964d60cb084522e3de8d92",q:o,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`https://pixabay.com/api/?${s}`).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()})}const p=document.querySelector(".search-form");p.addEventListener("submit",y);const l=document.querySelector(".loader");function y(o){o.preventDefault(),l.classList.remove("hidden");const s=o.target.elements.search.value.trim().toLowerCase();s?h(s).then(r=>{r.hits.length===0&&d.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",timeout:2e3,icon:""}),f(r.hits)}).catch(r=>console.log(r)).finally(()=>{o.target.reset(),l.classList.add("hidden")}):l.classList.add("hidden")}
 //# sourceMappingURL=commonHelpers.js.map
