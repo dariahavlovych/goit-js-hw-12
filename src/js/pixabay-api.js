43098f983590042e345433260333f6855cb88b7f
Daria Havlovych <daria.havlovych@gmail.com>
2024-07-29 23:09:29 +0300

    add async/await fix1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function getImagesByUserSearch(searchText) {
-  const response = await axios.get('https://pixabay.com/api', {
+  const response = await axios.get('https://pixabay.com/api/', {
     params: {
       key: '45077643-3bb964d60cb084522e3de8d92',
       q: searchText,
